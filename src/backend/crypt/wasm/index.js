755d62f15282f4b430fa70afafd5583135d321bb
Simon Arnell <14029547+simonarnell@users.noreply.github.com>
2024-10-25 16:00:02 +0100

    adding working example

--- src/backend/crypt/wasm/index.js
+++ src/backend/crypt/wasm/index.js
@@ -9,12 +9,29 @@ const wasi = new WASI({
   preopens: {},
 });
 
+const imports = wasi.getImportObject();
+
 (async () => {
   const wasm = await WebAssembly.compile(
-    await readFile('../../../../build/backend/vendor/libhydrogen/libhydrogen.wasm'),
+    await readFile('../../../../build/backend/vendor/libhydrogen/libhydrogen.wasm')           // Fetching our wasm built during npm install
   );
-  const instance = await WebAssembly.instantiate(wasm, wasi.getImportObject());
+  const instance = await WebAssembly.instantiate(wasm, imports);
+
+  wasi.start(instance);                                                                       // We must call a start method per WASI specification
+                                                                                              // Libhydrogen's main method is one we have patched to initialise it
+  const { hydro_random_uniform, hydro_hash_hash, memory } = instance.exports                  // Importing memory object and functions exported by the wasm
   
-  wasi.start(instance);
-  console.dir(instance.exports.hydro_random_uniform(20))
+  console.dir(hydro_random_uniform(20));                                                      // Testing a simple case of passing integers and fetching integers
+  
+  const dv = new DataView(memory.buffer);                                                     // DataView takes care of our platform specific endian conversions
+  console.log(dv)                                                                             // Visually checking the underlying arraybuffer is zero'd
+
+  const array = new Int32Array(dv.buffer, 0, 32)                                              // Creating a typed array as hydro_hash_hash expects i32s
+  const CONTEXT = "Examples";                                                                 // libhydrogen's namespacing concept
+  const MESSAGE = "Arbitrary data to hash";                                                   // Our message to be hashed
+
+  hydro_hash_hash(array.byteOffset, array.length, Buffer.from(MESSAGE),                       // Call the imported hashing function
+    Buffer.from(MESSAGE).length, Buffer.from(CONTEXT), null)
+  console.log(dv)                                                                             // Visually checking the underlying arraybuffer contains the hash
+
 })();
